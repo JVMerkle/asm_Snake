53c65ba2bd74412ea46f360d14b0b5eb0fe8d8c1
Julian Merkle <juli.merkle@gmail.com>
2017-06-02 13:59:55 +0200

    Initialize queue with 3 points

--- main.asm
+++ main.asm
@@ -1,12 +1,15 @@
-; Provisorische main
+; Initialize snake
+CALL queue_init
+MOV A, #0011011b ; Point (3,3)
+CALL queue_push
+MOV A, #0100011b ; Point (4,3)
+CALL queue_push
+MOV A, #00101011b ; Point (5,3)
+CALL queue_push
+
 main:
-	CALL queue_init
-	MOV A, #0xDE
-	CALL queue_push
-	CALL queue_pop
-loop:	
 	NOP
-	sjmp loop
+	sjmp main
 
 INCLUDE "queue.asm"
 
