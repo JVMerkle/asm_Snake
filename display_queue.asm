20b55bbfcd068b23fe2e6c2103b95014667f3b80
Julian Merkle <juli.merkle@gmail.com>
2017-06-03 12:37:48 +0200

    Add logic to retrieve the 3 axis bits from the queue

--- display_queue.asm
+++ display_queue.asm
@@ -1,3 +1,7 @@
+X_AXIS equ 21H
+Y_AXIS equ 22H
+
+; ()
 display:
 	MOV R0, QUEUE_TAIL
 	CALL add_to_display
@@ -10,6 +14,7 @@ iterate:
 	ADD A, #0x01
 	MOV 20H, R1
 	CJNE A, 20H, end_not_reached ; Check if reached head
+		LCALL render
 		RET ; END REACHED
 	end_not_reached:
 	CJNE A, #QUEUE_END + 0x01, in_bound ; Check queue bounds
@@ -22,4 +27,38 @@ iterate:
 ; (R0)
 add_to_display:
 	MOV A, @R0 ; Get value
+	MOV R1, A
+	CALL get_axis_x
+	MOV A, R2 ; Save x in R2
+	MOV R3, A
+	CALL get_axis_y
+	RET
+; R3 (R3)
+get_axis_byte:
+	
+	RET
+
+; R2 (R1)
+get_axis_x:
+	MOV A, R1
+	ANL A, #0xF0
+	RR A
+	RR A
+	RR A
+	RR A
+	MOV R2, A
+	RET
+
+; R2 (R1)
+get_axis_y:
+	MOV A, R1
+	ANL A, #0x0F
+	MOV R2, A
+	RET
+
+; ()
+; Displays calulated axises on the matrix
+render:
+	MOV P1, X_AXIS
+	MOV P0, Y_AXIS
 	RET
