782c4b55d5e927416c54e151d87c44b8562e4266
Julian Merkle <juli.merkle@gmail.com>
2017-06-06 21:21:08 +0200

    Add GNU GPLv3 header to each file

--- display.asm
+++ display.asm
@@ -1,3 +1,20 @@
+; Copyright 2017 Julian Merkle
+
+; This file is part of asm_Snake.
+
+; asm_Snake is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; asm_Snake is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with mMio.  If not, see <http://www.gnu.org/licenses/>.
+
 TMP_VAR equ 0x20
 X_AXIS equ 0x21
 Y_AXIS equ 0x22
@@ -36,7 +53,7 @@ add_to_display:
 	MOV Y_AXIS, R3
 	LCALL RENDER
 	RET
-	
+
 ; R3 (R2)
 ; Decode 3 bit axis representation to full byte
 get_axis_byte:
--- queue.asm
+++ queue.asm
@@ -1,3 +1,20 @@
+; Copyright 2017 Julian Merkle
+
+; This file is part of asm_Snake.
+
+; asm_Snake is free software: you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation, either version 3 of the License, or
+; (at your option) any later version.
+
+; asm_Snake is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+
+; You should have received a copy of the GNU General Public License
+; along with mMio.  If not, see <http://www.gnu.org/licenses/>.
+
 QUEUE_HEAD equ 0x30
 QUEUE_TAIL equ 0x31
 QUEUE_BEGIN equ 0x32
